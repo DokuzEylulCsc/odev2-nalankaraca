cd320297e090884c437532e1b62cea7ab7cce5c4
nalan karaca <55977697+nalankaraca@users.noreply.github.com>
2019-12-02 13:11:11 +0300

    Add files via upload

---
+++ nalanKaraca2018280068/nalanKaraca2018280068/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace nalanKaraca2018280068
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Roma = new System.Windows.Forms.Button();
+            this.sayi = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Roma
+            // 
+            this.Roma.Location = new System.Drawing.Point(411, 22);
+            this.Roma.Name = "Roma";
+            this.Roma.Size = new System.Drawing.Size(118, 23);
+            this.Roma.TabIndex = 0;
+            this.Roma.Text = "Roma rakamına çevir";
+            this.Roma.UseVisualStyleBackColor = true;
+            this.Roma.Click += new System.EventHandler(this.Roma_Click);
+            // 
+            // sayi
+            // 
+            this.sayi.Location = new System.Drawing.Point(411, 78);
+            this.sayi.Name = "sayi";
+            this.sayi.Size = new System.Drawing.Size(118, 23);
+            this.sayi.TabIndex = 1;
+            this.sayi.Text = "Sayıya Çevir";
+            this.sayi.UseVisualStyleBackColor = true;
+            this.sayi.Click += new System.EventHandler(this.sayi_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lütfen  [1,3999]  aralığında bir sayı giriniz";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(553, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lütfen [I,MMM CM XC IX] aralığında  roma rakamı giriniz";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(553, 78);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 210);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sayi);
+            this.Controls.Add(this.Roma);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Roma;
+        private System.Windows.Forms.Button sayi;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ nalanKaraca2018280068/nalanKaraca2018280068/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nalanKaraca2018280068
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Roma_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            int sayi = int.Parse(textBox1.Text);
+            int birler=0, onlar=0, yuzler=0, binler=0;
+            if (sayi<10)
+            {
+                birler = sayi;
+            }
+            else if (sayi>=10 && sayi<100)
+            {
+                birler=sayi%10;
+                onlar = sayi / 10;
+            }
+            else if (sayi>=100 && sayi<1000)
+            {
+                birler = sayi % 10;
+                onlar = (sayi % 100) / 10;
+                yuzler = sayi / 100;
+            }
+            else
+            {
+                birler = sayi % 10;
+                onlar = ((sayi % 1000) % 100) / 10;
+                yuzler = (sayi % 1000) / 100;
+                binler = sayi / 1000;
+            }
+            string[] birlerR = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] onlarR = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] yuzlerR = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] binlerR = { "", "M", "MM", "MMM" };
+            textBox2.Text += binlerR[binler] + " ";
+            textBox2.Text += yuzlerR[yuzler] + " ";
+            textBox2.Text += onlarR[onlar];
+            textBox2.Text += " " + birlerR[birler];
+
+        }
+
+        private void sayi_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            string roma = textBox3.Text.ToUpper();
+            int sayi = 0, a1 = 0, a2 = 0, a3 = 0;
+            for (int i = 0; i < roma.Length; i++)
+            {
+                a2 = a1;
+                switch (roma[i])
+                {
+                    case 'I':a1 = 1;break;
+                    case 'V': a1 = 5; break;
+                    case 'X': a1 = 10; break;
+                    case 'L': a1 = 50; break;
+                    case 'C': a1 = 100; break;
+                    case 'D': a1 = 500; break;
+                    case 'M': a1 = 1000; break;
+
+                }
+                if (a1>a2)
+                {
+                    a3 = -2 * a2;
+                }
+                else
+                {
+                    a3 = 0;
+                }
+                sayi = sayi + a1 + a3;
+            }
+            if (sayi<3999)
+            {
+                textBox4.Text = sayi.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Sayı geçerli aralıkta değil");
+            }
+            textBox3.Clear();
+        }
+        //https://mustafabukulmez.com/2019/01/05/c-girilen-sayiyi-roma-rakamina-cevirmek/
+        //https://www.youtube.com/watch?v=Omwh5981X2c
+
+    }
+}
